7c17f64b5c83b3a231d4555cebfad1c50aeebcc1
Andrés Domínguez <andres_gomez@live.com.mx>
2015-12-19 01:33:29 -0600

    Agregue un error en lineas <?php

--- conexion.php
+++ conexion.php
@@ -1,4 +1,4 @@
-<?
+<?php
 //$link=mysql_connect("138.128.189.242", "revistet_html", "platzi.com123");
 //mysql_select_db("revistet_html",$link) OR DIE ("Error: No es posible establecer la conexión");
 $db_host="138.128.189.242";
