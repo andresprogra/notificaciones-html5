4470bd18c44499ae426fb75888fa7e0e3c4d9e00
Andrés Domínguez <andres_gomez@live.com.mx>
2015-12-19 12:15:13 -0600

    dbbb

--- index.php
+++ index.php
@@ -87,9 +87,33 @@
 				<input type="submit" class="btn" id="boton" />
 			</form>
 		</section>
+		<?php
+
+
+
+			$name = $_POST['name'];
+			$last = $_POST['last'];
+			$pais = $_POST['pais'];
+			$comentario = $_POST['comentario'];
+
+
+
+			$conn_string = "host=ec2-107-21-224-11.compute-1.amazonaws.com port=5432 dbname=delt70p4uoagga user=bdodcgxjiyfxvh password=wDEMh9FzNIWOMWRBs31w8n18uB";
+			$conexion = pg_connect($conn_string);
+			//conectarse a una base de datos llamada "test" en el host "sheep" con el nombre de usuario y password
+
+			$result1 = pg_query($conexion, "SELECT COUNT(*) FROM notificaciones;");
+
+
+		?>
+
+
 		<section class="col-xs bg row">
 			<div class="tabla-nombre col-xs">
 				<h3 class="col-xs" id="nombrealumno">Nombre</h3>
+				<?php for ($i=0; $i < $result1 - 1 ; $i++) {  ?>
+					<label id="Label-nuevo">andres</label>
+				<?php } ?>
 			</div>
 			<div class="tabla-apellidos col-xs">
 				<h3 class="col-xs" id="apellidoalumno">Apellido</h3>
