2f5c766512e72b96356d65577afd75108fc5d47f
Andrés Domínguez <andres_gomez@live.com.mx>
2015-12-19 10:27:20 -0600

    db

--- user.php
+++ user.php
@@ -1,18 +1,13 @@
 <?php
-$db_host="138.128.189.242";
-$db_user="revistet_html";
-$db_password="platzi.com123";
-$db_name="revistet_html";
-$db_table_name="notificaciones";
-//$db_table_name="nombre_de_tabla";
-$db_connection = mysql_connect($db_host, $db_user, $db_password);
+
+
 
 $name = $_POST['name'];
 $last = $_POST['last'];
 $pais = $_POST['pais'];
 $comentario = $_POST['comentario'];
 
-$link = mysql_connect("andresdominguez.mx", "revistet_html", "platzi.com123");
+$link = mysql_connect("andresdominguez.mx", "revistet_html", "platzi.com123") or die ('Mal');
 mysql_select_db("revistet_html",$link);
 $sql = "INSERT INTO notificaciones (nombre, last, pais, comentario) ".
      "VALUES ('$nombre', '$last', '$pais', '$comentario')";
