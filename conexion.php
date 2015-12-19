96a0576173d9df616beb3a7e91c0f11068c9f93b
Andrés Domínguez <andres_gomez@live.com.mx>
2015-12-19 01:22:04 -0600

    modifique la conexion

--- conexion.php
+++ conexion.php
@@ -1,4 +1,11 @@
 <?
-$link=mysql_connect("138.128.189.242", "revistet_html", "platzi.com123");
-mysql_select_db("revistet_html",$link) OR DIE ("Error: No es posible establecer la conexión");
+//$link=mysql_connect("138.128.189.242", "revistet_html", "platzi.com123");
+//mysql_select_db("revistet_html",$link) OR DIE ("Error: No es posible establecer la conexión");
+$db_host="138.128.189.242";
+$db_user="revistet_html";
+$db_password="platzi.com123";
+$db_name="revistet_html";
+//$db_table_name="nombre_de_tabla";
+$db_connection = mysql_connect($db_host, $db_user, $db_password);
+
 ?>
